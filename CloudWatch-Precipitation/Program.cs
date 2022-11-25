d5b666d9eb2173b1927dcbc710312f4ef2925934
Dubem Otekeiwebia <dondubbie007@gmail.com>
2022-11-25 17:13:09 +0100

    Add initial deps

--- CloudWatch-Precipitation/Program.cs
+++ CloudWatch-Precipitation/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+{
+    return Results.Ok(zip);
+});
 
 app.Run();
